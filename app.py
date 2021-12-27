b3d06eb4e18fcdb15d322971c618d8c46977af33
Pierobon <gabriel.pierobon@capgemini.com>
2021-12-27 10:15:07 +0100

    moved link to youtube channel

--- app.py
+++ app.py
@@ -86,10 +86,12 @@ model = model_dict[model_selected]['MODEL']
 
 # Print some description of the selected model to the user
 col1, col2, col3 = st.columns(3)
-col1.write(f"""**Model Family**:   
+col1.write(f"""**Model Family**
                                 {model_dict[model_selected]['MODEL FAMILY']}""")
-col2.write(f"**Model Question**: {model_dict[model_selected]['MODEL QUESTION']}")
-col3.write(f"**Model Description**: {model_dict[model_selected]['MODEL DESCRIPTION']}")
+col2.write(f"""**Model Question**
+                                {model_dict[model_selected]['MODEL QUESTION']}""")
+col3.write(f"""**Model Description**  
+                                {model_dict[model_selected]['MODEL DESCRIPTION']}""")
 
 # Source Selection
 source_selection = st.sidebar.radio('Select a new card!', ['FROM JSON', 'USE FORM', 'FROM DB'])
